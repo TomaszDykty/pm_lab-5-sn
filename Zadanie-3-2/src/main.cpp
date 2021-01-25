05077509dfedb5dbba335392261702da7d1ea8b5
Tomasz <tomasz.dykty@student.wat.edu.pl>
2021-01-25 09:31:49 +0100

    wykonane zadani 3-2-1

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,10 +1,40 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
+void dispMenu (void) ;
+int menu = 1;
 
 void setup() {
-  // zadanie sprawdzające
+  lcd.begin(16, 2);
+  lcd.print("Tomasz Dykty");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  dispMenu ();
+  delay (1000);
+  menu++ ;
+  if(menu>3) 
+  {menu=1;}
+ 
+  
+}
+
+void dispMenu (void) {
+  switch (menu)
+  {
+  case 1:
+    lcd.setCursor(0,0);
+    lcd.print("Menu 1");
+    break;
+  case 2:
+    lcd.setCursor(0,0);
+    lcd.print("Menu 2");
+    break;
+  case 3:
+    lcd.setCursor(0,0);
+    lcd.print("Menu 3");
+    break;
+  
+  }
+
 }

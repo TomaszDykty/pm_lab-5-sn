f4255d51e80892b4a51eae77d7f58418bce739a8
Tomasz <tomasz.dykty@student.wat.edu.pl>
2021-01-25 10:05:53 +0100

    zadanie 3-2-1 bez milis będzie zmieniane

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,21 +1,24 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#define butonUp 11
+#define butonDown 13
+
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 void dispMenu (void) ;
+
+void changeMenu (void);
 int menu = 1;
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("Tomasz Dykty");
+pinMode (butonUp,INPUT_PULLUP);
+pinMode (butonDown,INPUT_PULLUP);
+  //lcd.print("Tomasz Dykty");
 }
 
 void loop() {
   dispMenu ();
-  delay (1000);
-  menu++ ;
-  if(menu>3) 
-  {menu=1;}
- 
+  changeMenu();
   
 }
 
@@ -35,6 +38,27 @@ void dispMenu (void) {
     lcd.print("Menu 3");
     break;
   
+  }
+
+}
+
+
+
+void changeMenu (void)
+{
+  if (digitalRead(butonUp) == HIGH)
+  {
+    ++menu ;
+    if(menu>3) 
+    {menu=1;}
+  //  delay(500);  zmień na funkcje milis 0,5s
+  }
+  if (digitalRead(butonDown) == HIGH)
+  {
+    --menu ;
+    if(menu<1) 
+    {menu=3;}
+   // delay(500);
   }
 
 }

a9d31989f1de142b2a29ef671204ea2529b746d0
Tomasz <tomasz.dykty@student.wat.edu.pl>
2021-01-25 13:47:51 +0100

    zadanie 3-2-4 wykonano zadanie

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -7,29 +7,54 @@ LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
 void dispMenu (void) ;
 
 void changeMenu (void);
+void readTimperature (void);
+
 int menu = 1;
 bool psButonUp = HIGH;
 bool psButonDown = HIGH;
+
+float temperature = 0.0f;
+/*
+byte stopnie[8] = {                //konfigurajca znaku stopnie
+  0b00111,
+  0b00101,
+  0b00111,
+  0b00000,
+  0b00000,    //nie działało poprawnie podczas symulacji
+  0b00000,
+  0b00000,
+  0b00000
+};
+*/
 
 void setup() {
   lcd.begin(16, 2);
 pinMode (butonUp,INPUT_PULLUP);
 pinMode (butonDown,INPUT_PULLUP);
+
+//lcd.createChar(1, stopnie); //nie działało poprawnie podczas symulacji
   //lcd.print("Tomasz Dykty");
 }
 
 void loop() {
   dispMenu ();
   changeMenu();
-  
+  readTimperature();
 }
 
 void dispMenu (void) {
+  
   switch (menu)
   {
   case 1:
     lcd.setCursor(0,0);
-    lcd.print("Menu 1");
+    lcd.print("Temperatura:");
+    lcd.setCursor(0,1);
+    lcd.print(temperature);
+    lcd.setCursor(6,1);
+   // lcd.print((char)1); //nie działało poprawnie podczas symulacji ze względu na zakłucenia i błędy w co 4 symulacji usunełem
+    lcd.print("C");
+    
     break;
   case 2:
     lcd.setCursor(0,0);
@@ -54,7 +79,7 @@ void changeMenu (void)
     ++menu ;
     if(menu>3) 
     {menu=1;}
-  
+   lcd.clear();
   }
   if (digitalRead(butonDown) == HIGH && psButonDown == LOW)
   {
@@ -62,9 +87,18 @@ void changeMenu (void)
     --menu ;
     if(menu<1) 
     {menu=3;}
-   
+   lcd.clear();
   }
   psButonUp = digitalRead(butonUp);
   psButonDown = digitalRead(butonDown);
+
+}
+
+void readTimperature (void)
+{
+  unsigned int digital = analogRead(A5);
+  float resolution = (5.0f / 1024.0f);
+  float voltage = resolution * digital;
+  temperature = (voltage-0.1f) *(125.0f+40.0f) / (1.75f-0.1f)-40.0f;
 
 }
